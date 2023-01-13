65eb9e56b06673c2ee70b5784c4c2d2ecb3d8606
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 21:10:51 +0100

    Retirando o Banner

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
-import Banner from 'componentes/Banner';
 import styles from './Inicio.module.css';
 import posts from 'json/posts.json';
 import Post from 'componentes/Post';
 
 export default function Inicio() {
-  return (
-    <main>
-      <Banner />
+  return (    
       <ul className={styles.posts}>
         {posts.map((post)=> {
           return <li key={post.id}><Post post={post} /></li>
         })}
       </ul>
-    </main>
+    
     
   )
 }

687a257d0668cd39a23f3af53dd9efc91cf4e3d1
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 16:04:42 +0100

    iniciando comp Menu

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MenuLink from '../MenuLink';
+import styles from './Menu.module.css';
+
+export default function Menu() {
+  return (
+    <header>
+      <nav className={styles.navegacao}>
+        <MenuLink to="/">Inicio</MenuLink>
+        <MenuLink to="/sobremim">Sobre Mim</MenuLink>
+
+      </nav>
+    </header>
+  )
+}

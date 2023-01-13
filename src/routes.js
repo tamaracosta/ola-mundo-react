cac5243bac451c7b1848f8b7a711b7ed08324fe8
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 16:03:39 +0100

    mudando nome do arquivo para Routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
-      
+      <Menu/>
 
       <Routes>
         <Route path="/" element={<Inicio />}></Route>
-        <Route path="sobremim" element={<SobreMim />}></Route>
+        <Route path="/sobremim" element={<SobreMim />}></Route>
         <Route path="*" element={<div><h1>Não encontrada</h1></div>}></Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App;
+export default AppRoutes;

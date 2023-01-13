9dae3d09d1d2f32e0e72e8a9c1c0177359ce4d8c
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 21:10:23 +0100

    Pagina Padrao - Banner

---
+++ src/paginas/PaginaPadrao/index.js
@@ -0,0 +1,15 @@
+import Banner from 'componentes/Banner'
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function PaginaPadrao() {
+  return (
+    <>
+      <Banner />      
+      <Outlet />
+    </>
+    
+
+  )
+}

811a664abb2fc5d9688260e01a714682bb2f2fbc
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 21:56:34 +0100

    PostCard

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "paginas/PaginaPadrao";
+import Post from "paginas/Post";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
@@ -15,6 +16,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />}></Route>
           <Route path="sobremim" element={<SobreMim />}></Route>
+          <Route path="post/:id" element={<Post />}></Route>
         </Route>
 
         <Route path="*" element={<div><h1>Não encontrada</h1></div>}></Route>

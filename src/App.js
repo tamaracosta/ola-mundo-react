f9c6ff3d5a17f748ed1ef27188be01455d624d65
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 14:51:36 +0100

    Começando Rotas Inicio e Sobre Mim

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      
+
+      <Routes>
+        <Route path="/" element={<Inicio />}></Route>
+        <Route path="sobremim" element={<SobreMim />}></Route>
+        <Route path="*" element={<div><h1>Não encontrada</h1></div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;

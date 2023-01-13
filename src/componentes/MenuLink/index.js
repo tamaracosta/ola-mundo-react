137dd039f83f3734726ab9fefc7405e1c0d88172
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 16:04:15 +0100

    criação MenuLink usando NavLink - destacar link ativo

---
+++ src/componentes/MenuLink/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './MenuLink.module.css';
+
+export default function MenuLink({ children, to }) {
+  
+  return (
+    <NavLink
+      className={({ isActive }) => `
+          ${styles.link}
+          ${isActive ? styles.linkDestacado : ""}
+      `}
+      to={to}
+      end
+    >
+      {children}
+    </NavLink>
+  )
+}

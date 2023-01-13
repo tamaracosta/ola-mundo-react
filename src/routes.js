ae2c396aa7b833d83fd359ce9069719ced94ebe9
tamaracosta <tamarasantosc@gmail.com>
2023-01-13 21:11:15 +0100

    Sobre Mim, arrumando rotas

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "paginas/PaginaPadrao";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
@@ -10,9 +11,14 @@ function AppRoutes() {
       <Menu/>
             
       <Routes>
-        <Route path="/" element={<Inicio />}></Route>
-        <Route path="/sobremim" element={<SobreMim />}></Route>
+
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="sobremim" element={<SobreMim />}></Route>
+        </Route>
+
         <Route path="*" element={<div><h1>Não encontrada</h1></div>}></Route>
+        
       </Routes>
       <Rodape />
     </BrowserRouter>
